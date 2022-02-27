86e10a2d7936bc8f7594ccd067de3c054bc7e558
Dragomir Petkov <dramppet@abv.bg>
2022-02-27 15:37:31 +0200

    add-first

---
+++ Python_OOP/1first_Steps/ex/1shop.py
@@ -0,0 +1,14 @@
+import items as items
+
+
+class Shop:
+    def __init__(self,name, items):
+        self.name = name
+        self.items = items
+
+    def get_items_count(self):
+        return f'{len(self.items)}'
+
+
+shop = Shop("My Shop", ["Apples", "Bananas", "Cucumbers"])
+print(shop.get_items_count())
