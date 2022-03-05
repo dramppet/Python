af207439b49e66fb45242da869eb0703bbc349aa
Dragomir Petkov <dramppet@abv.bg>
2022-03-05 20:13:11 +0200

    add-lab-five

--- Python_OOP/4Encapsylation/lab/5Account.py
+++ Python_OOP/4Encapsylation/lab/5Account.py
@@ -9,9 +9,14 @@ class Account:
             return self.__id
         return 'Wrong pin'
 
-account = Account(8827312, 100, 3421)
-print(account.get_id(1111))
-print(account.get_id(3421))
-print(account.balance)
-# print(account.change_pin(2212, 4321))
-# print(account.change_pin(3421, 1234))
+    def change_pin(self, old_pin, new_pin):
+        if old_pin == self.__pin:
+            self.__pin = new_pin
+            return 'Pin changed'
+        return 'Wrong pin'
+
+
+
+
+a = Account(1234, 44, 4444)
+print(a.change_pin(4444, 1234))
