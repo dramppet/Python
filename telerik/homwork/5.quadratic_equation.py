1be4e033100c319cfcdca664e56aa8839831559f
Dragomir Petkov <dramppet@abv.bg>
2022-04-03 19:53:31 +0300

    add

---
+++ telerik/homwork/5.quadratic_equation.py
@@ -0,0 +1,12 @@
+from math import sqrt
+
+a = float(input())
+b = float(input())
+c = float(input())
+
+discriminant = pow(b,2) - 4 * a * c
+
+x1 = (- b - sqrt(discriminant)) / (2 * a)
+x2 = (- b + sqrt(discriminant)) / (2 * a)
+
+print(f'x1 = {x1}; x2 = {x2}')
