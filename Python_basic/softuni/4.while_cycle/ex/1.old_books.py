f085e4755edfe1ee31128a73235ce99ac5b344b3
Dragomir Petkov <dramppet@abv.bg>
2022-04-07 21:16:54 +0300

    add

--- Python_basic/softuni/4.while_cycle/ex/1.old_books.py
+++ Python_basic/softuni/4.while_cycle/ex/1.old_books.py
@@ -2,15 +2,17 @@ book_name = input()
 
 curr_book = input()
 counter = 0
-while not curr_book == book_name:
+book_is_found = False
+while not curr_book == 'No More Books':
 
-    if curr_book == 'No More Books':
+    if curr_book == book_name:
+        book_is_found = True
         break
 
     counter += 1
     curr_book = input()
 
-if curr_book == book_name:
+if book_is_found:
     print(f'You checked {counter} books and found it.')
 else:
     print('The book you search is not here!')
