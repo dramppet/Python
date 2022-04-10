d22d5336deabaf04045913eb45685541e04447e7
Dragomir Petkov <dramppet@abv.bg>
2022-04-10 12:11:13 +0300

    add

--- telerik/2.onditional_statements/lection/proba.py
+++ telerik/2.onditional_statements/lection/proba.py
@@ -1,11 +1,15 @@
 number = int(input())
 
+# option1
+
 # if number % 2 == 0:
 #     result = 'even'
 # else:
 #     result = 'odd'
 #
 # print(result)
+
+#option 2
 
 result = 'even' if number % 2 == 0 else 'odd'
 
