c5344a2ae0116ec4aabd4fb198c92a6c16563335
Dragomir Petkov <dramppet@abv.bg>
2022-04-10 13:09:12 +0300

    add

--- telerik/homwork/5.quadratic_equation.py
+++ telerik/homwork/5.quadratic_equation.py
@@ -9,4 +9,4 @@ discriminant = pow(b,2) - 4 * a * c
 x1 = (- b - sqrt(discriminant)) / (2 * a)
 x2 = (- b + sqrt(discriminant)) / (2 * a)
 
-print(f'x1 ={x1}; x2 ={x2}')
+print(f'x1 ={x1:g}; x2 ={x2:g}')
