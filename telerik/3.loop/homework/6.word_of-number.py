f0bd3f0f5696926bf1b4afc94dd761bec770bc97
Dragomir Petkov <dramppet@abv.bg>
2022-04-23 09:02:49 +0300

    add

--- telerik/3.loop/homework/6.word_of-number.py
+++ telerik/3.loop/homework/6.word_of-number.py
@@ -1,11 +1,10 @@
 text_input = input()
 
-number_find = '0123456789'
-
 for num in text_input:
-    if num in number_find:
+    if num >= '0' and num <= '9' or num == '-':
         print(float(text_input) + 1)
         break
     else:
         print(text_input[::-1])
         break
+
