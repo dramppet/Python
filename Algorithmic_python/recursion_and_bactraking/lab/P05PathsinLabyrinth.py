e122fdfcfa7589d4007011d82287230b9d71aae8
Dragomir Petkov <dramppet@abv.bg>
2022-07-10 08:33:55 +0300

    add

--- Algorithmic_python/recursion_and_bactraking/lab/P05PathsinLabyrinth.py
+++ Algorithmic_python/recursion_and_bactraking/lab/P05PathsinLabyrinth.py
@@ -1,3 +1,18 @@
+def find_path(row, col, matrix):
+
+    if row < 0 or col < 0 or matrix[row] > 0 or matrix[row][col] > 0:
+        return
+
+    if matrix[row][col] == 'e':
+        pass
+    if matrix[row][col] == '*':
+        return
+
+    find_path(row, col + 1,matrix)
+    find_path(row, col - 1,matrix)
+    find_path(row + 1, col,matrix)
+    find_path(row - 1, col,matrix)
+
 row = int(input())
 col = int(input())
 
@@ -6,3 +21,4 @@ lab = []
 for _ in range(row):
     lab.append(input())
 
+find_path(0, 0, lab)
