101eca7ff41ce0f6ecf583b98afb3814f2ee93d8
Dragomir Petkov <dramppet@abv.bg>
2022-07-10 15:26:03 +0300

    add

--- Algorithmic_python/recursion_and_bactraking/lab/P01RecursiveArraySum.py
+++ Algorithmic_python/recursion_and_bactraking/lab/P01RecursiveArraySum.py
@@ -1,1 +1,8 @@
+def sum_array(num, idx):
+    if idx == len(num) - 1:
+        return num[idx]
+    return num[idx] + sum_array(num, idx + 1)
+
 nums = [int(x) for x in input().split()]
+
+print(sum_array(nums, 0))
