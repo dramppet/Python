fcfc6f68adb0bfa3f67774aaa3ab558423ab130f
Dragomir Petkov <dramppet@abv.bg>
2022-07-11 20:13:02 +0300

    add

---
+++ Algorithmic_python/recursion_and_bactraking/ex/P02nestedLoopsToRecursion.py
@@ -0,0 +1,15 @@
+def gen_vector(idx,vector,n):
+    if idx >= len(vector):
+        print(*vector)
+        return
+
+    for num in range(1,n + 1):
+        vector[idx] = num
+
+        gen_vector(idx + 1,vector,n)
+
+n = int(input())
+
+vector = [None] * n
+
+gen_vector(0,vector,n)
