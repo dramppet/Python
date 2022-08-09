401dce166426aea26996118335ffcedc1e1a5a3d
Dragomir Petkov <dramppet@abv.bg>
2022-08-09 10:20:49 +0300

    add

--- Algorithmic_python/P04GraphsShortestPathandMST/lab/P02Dijkstra'sAlgorithm.py
+++ Algorithmic_python/P04GraphsShortestPathandMST/lab/P02Dijkstra'sAlgorithm.py
@@ -1,8 +1,12 @@
+from queue import PriorityQueue
+
+
 class Edge:
     def __init__(self, source, destination, weight):
         self.source = source
         self.destination = destination
         self.weight = weight
+
 
 edges = int(input())
 graph = {}
@@ -20,5 +24,23 @@ target = int(input())
 
 max_node = max(graph.keys())
 
-distance = [0] * (max_node + 1)
+distance = [float('inf')] * (max_node + 1)
 parent = [None] * (max_node + 1)
+
+distance[start] = 0
+
+pq = PriorityQueue()
+pq.put((0, start))
+
+while not pq.empty():
+    min_distance, node = pq.get()
+
+    if node == target:
+        break
+
+    for edge in graph[node]:
+        new_distance = min_distance + edge.weight
+
+        if new_distance < distance[edge.destination]:
+            distance[edge.destination] = new_distance
+            parent[edge.destination] = node
