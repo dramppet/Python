1048a9141fa6649e16bd118559d60c579a98f9a7
Dragomir Petkov <dramppet@abv.bg>
2022-08-10 10:22:22 +0300

    add

---
+++ Algorithmic_python/P03IntroductionToGraph/lab/Source_Removal_Topological_Sorting_DFS.py
@@ -0,0 +1,37 @@
+from collections import  deque
+
+def dfs(node, graph, visited, cycles, sorted_nodes):
+    if node in cycles:
+        raise Exception
+
+    if node in visited:
+        return
+
+    visited.add(node)
+    cycles.add(node)
+
+    for child in graph[node]:
+        dfs(child, graph, visited, cycles, sorted_nodes)
+
+    cycles.remove(node)
+    sorted_nodes.appendleft(node)
+
+
+nodes = int(input())
+
+graph = {}
+
+for _ in range(nodes):
+    lin_parts = input().split('->')
+    node = lin_parts[0].strip()
+    children = lin_parts[1].strip().split(', ') if lin_parts[1] else []
+    graph[node] = children
+
+visited = set()
+cycles = set()
+sorted_nodes = deque()
+
+for node in graph:
+    dfs(node, graph, visited, cycles, sorted_nodes)
+
+print(*sorted_nodes)
