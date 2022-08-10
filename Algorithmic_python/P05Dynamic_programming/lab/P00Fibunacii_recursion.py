9b20f65a7b6952af4ae93f56b5c00f4621a276b8
Dragomir Petkov <dramppet@abv.bg>
2022-08-10 08:25:38 +0300

    add

--- Algorithmic_python/P05Dynamic_programming/lab/P00Fibunacii_recursion.py
+++ Algorithmic_python/P05Dynamic_programming/lab/P00Fibunacii_recursion.py
@@ -3,11 +3,10 @@ def calc_fib(n, memo):
         return memo[n]
     if n <= 2:
         return 1
-
     result = calc_fib(n - 1, memo) + calc_fib(n - 2, memo)
     memo[n] = result
     return result
 
-n = int(input())
+number = int(input())
 
-print(calc_fib(n, {}))
+print(calc_fib(number, {}))
