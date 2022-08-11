cb4c0a9083bd725c937364f4520c0ceaf31e2b17
Dragomir Petkov <dramppet@abv.bg>
2022-08-11 13:55:43 +0300

    add

--- Algorithmic_python/Exsam_Prep/P01TheStoryTelling.py
+++ Algorithmic_python/Exsam_Prep/P01TheStoryTelling.py
@@ -1,3 +1,15 @@
+from collections import deque
+
+def dfs(node, graph, visited, result):
+    if node in visited:
+        return
+    visited.add(node)
+
+    for child in graph[node]:
+        dfs(child, graph, visited, result)
+
+    result.appendleft(node)
+
 graph = {}
 
 while True:
@@ -7,3 +19,16 @@ while True:
         break
 
     node, children_str = [x.strip() for x in line.split('->')]
+    children = children_str.split()
+    graph[node] = children
+
+visited = set()
+
+result = deque()
+
+for node in graph:
+    dfs(node, graph, visited, result)
+
+print(*result, sep = ' ')
+
+
