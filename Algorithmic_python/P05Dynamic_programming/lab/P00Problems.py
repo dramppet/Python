cd872f88fed04171b19eb8698eaf0d613c663a21
Dragomir Petkov <dramppet@abv.bg>
2022-08-12 12:12:30 +0300

    add

--- Algorithmic_python/P05Dynamic_programming/lab/P00Problems.py
+++ Algorithmic_python/P05Dynamic_programming/lab/P00Problems.py
@@ -1,14 +1,17 @@
-def calc_fib(num, memo):
-    if num in memo:
-        return memo[num]
-    if num == 0:
+def calc_fib(n, memo):
+    if n in memo:
+        return memo[n]
+
+    if n == 0:
         return 0
-    elif num == 1:
+    if n == 1:
         return 1
-    result = calc_fib(num - 1, memo) + calc_fib(num - 2, memo)
-    memo[num] = result
+
+    result = calc_fib(n - 1, memo) + calc_fib(n - 2, memo)
+    memo[n] = result
+
     return result
 
-n = int(input())
+num_fib = int(input())
 
-print(calc_fib(n, {}))
+print(calc_fib(num_fib, {}))
