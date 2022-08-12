6021a1ea8bcadaf46c1974854c86673d771d6193
Dragomir Petkov <dramppet@abv.bg>
2022-08-12 13:14:13 +0300

    add

--- Algorithmic_python/P05Dynamic_programming/lab/P00Problems.py
+++ Algorithmic_python/P05Dynamic_programming/lab/P00Problems.py
@@ -1,11 +1,20 @@
 rows = int(input())
 cols = int(input())
 
+matrix = []
 dp = []
 
 for _ in range(rows):
-    line = [int(x) for x in input().split()]
-    dp.append(line)
+    matrix.append([int(x) for x in input().split()])
+    dp.append([0] * cols)
+
+dp[0][0] = matrix[0][0]
+
+for col in range(1, cols):
+    dp[0][col] = dp[0][col - 1] + matrix[0][col]
+
+for row in range(1, rows):
+    dp[row][0] = dp[row - 1][0] + matrix[row][0]
 
 
 
