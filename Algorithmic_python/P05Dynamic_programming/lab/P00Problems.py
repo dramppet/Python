be53de661547dea74631e9b6de0e2477080334b2
Dragomir Petkov <dramppet@abv.bg>
2022-08-12 13:52:16 +0300

    add

--- Algorithmic_python/P05Dynamic_programming/lab/P00Problems.py
+++ Algorithmic_python/P05Dynamic_programming/lab/P00Problems.py
@@ -1,43 +1,11 @@
-from collections import deque
-rows = int(input())
-cols = int(input())
+first = input()
+second = input()
 
-matrix = []
-dp = []
+rows = len(first) + 1
+cols  = len(second) + 1
 
-for _ in range(rows):
-    matrix.append([int(x) for x in input().split()])
-    dp.append([0] * cols)
+dp= []
 
-dp[0][0] = matrix[0][0]
+[dp.append([0] * cols) for _ in range(rows)]
 
-for col in range(1, cols):
-    dp[0][col] = dp[0][col - 1] + matrix[0][col]
 
-for row in range(1, rows):
-    dp[row][0] = dp[row - 1][0] + matrix[row][0]
-
-for row in range(1, rows):
-    for col in range(1, cols):
-        dp[row][col] = max(dp[row - 1][col], dp[row][col - 1]) + matrix[row][col]
-
-row = rows - 1
-col = cols - 1
-result = deque()
-
-while row > 0 and col > 0:
-    result.appendleft([row,col])
-    if dp[row][col - 1] >= dp[row - 1][col]:
-        col -= 1
-    else:
-        row -= 1
-
-while row > 0:
-    result.appendleft([row,col])
-    row -= 1
-
-while col > 0:
-    result.appendleft([row,col])
-    col -= 1
-result.appendleft([0, 0])
-print(*result, sep = ' ')
