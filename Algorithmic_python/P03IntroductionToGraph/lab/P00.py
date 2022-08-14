bbc1dacc36dabc61fd0caa65fa269500cfe3e662
Dragomir Petkov <dramppet@abv.bg>
2022-08-14 12:27:48 +0300

    add

--- Algorithmic_python/P03IntroductionToGraph/lab/P00.py
+++ Algorithmic_python/P03IntroductionToGraph/lab/P00.py
@@ -1,31 +1,14 @@
-from collections import deque
-
-
-def dfs(node, graph, visite, com):
-    if node in visite:
-        return
-
-    visite.add(node)
-
-    for child in graph[node]:
-        dfs(child, graph, visite)
-    com.appendleft(node)
-
-
 nodes = int(input())
 
-graph = {}
+graph = []
 
 for node in range(nodes):
     line_str = input()
 
     if len(line_str) > 0:
-        graph[node] = [int(x) for x in line_str.split()]
+        graph.append([int(x) for x in line_str.split()])
     else:
-        graph[node] = []
+        graph.append([])
 
 visited = set()
-component = deque()
 
-for node in graph:
-    dfs(node, graph, visited, component)
