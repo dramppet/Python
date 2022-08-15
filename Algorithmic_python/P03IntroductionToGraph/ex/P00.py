b1da8404e8bc070b36f25c8c75c95fe35c1325fd
Dragomir Petkov <dramppet@abv.bg>
2022-08-15 09:45:06 +0300

    add

--- Algorithmic_python/P03IntroductionToGraph/ex/P00.py
+++ Algorithmic_python/P03IntroductionToGraph/ex/P00.py
@@ -1,11 +1,29 @@
+def dfs(parent, row, col, matrix, visited):
+    if visited[row][col]:
+        return
+
+    if matrix[row][col] != parent:
+        return
+
+
+
 rows = int(input())
 cols = int(input())
 
 matrix = []
+visited = []
 
 for _ in range(rows):
     matrix.append(list(input()))
+    visited.append([False] * cols)
+
+
+
+
 
 for row in range(rows):
     for col in range(cols):
-        pass
+        if visited[row][col]:
+            continue
+        key = matrix[row][col]
+        dfs(key, row, col, matrix, visited)
