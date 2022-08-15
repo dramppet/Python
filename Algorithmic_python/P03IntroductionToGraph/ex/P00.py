d463cfd280c5d5616bd47bd80294946e9fe3d764
Dragomir Petkov <dramppet@abv.bg>
2022-08-15 11:19:17 +0300

    add

--- Algorithmic_python/P03IntroductionToGraph/ex/P00.py
+++ Algorithmic_python/P03IntroductionToGraph/ex/P00.py
@@ -1,37 +1,11 @@
-def dfs(node, graph, visited, cycles):
-    if node in cycles:
-        raise Exception
-    if node in visited:
-        return
+nodes = int(input())
 
-    visited.add(node)
-    cycles.add(node)
+graph = []
 
-    for child in graph[node]:
-        dfs(child, graph, visited, cycles)
-
-    cycles.remove(node)
-
-graph = {}
-while True:
+for _ in range(nodes):
     line = input()
-
-    if line == 'End':
-        break
-
-    source, destination = input().split('-')
-    if source not in graph:
-        graph[source] = destination
-    if destination not in graph:
-        graph[destination] = []
-    graph[source].append(destination)
-
-visited = set()
-try:
-    for node in graph:
-        dfs(node, graph, visited, set())
-    print('Yes')
-except Exception:
-    print('No')
-
-
+    children = []
+    for idx, state in enumerate(children):
+        if state in 'Y':
+            children.append(idx)
+    graph.append(children)
