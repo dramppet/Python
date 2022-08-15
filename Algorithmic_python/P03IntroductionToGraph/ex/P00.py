63925a986ed1011dbe303e930b4b4dbc1b9db83c
Dragomir Petkov <dramppet@abv.bg>
2022-08-15 09:55:29 +0300

    add

--- Algorithmic_python/P03IntroductionToGraph/ex/P00.py
+++ Algorithmic_python/P03IntroductionToGraph/ex/P00.py
@@ -1,10 +1,18 @@
 def dfs(parent, row, col, matrix, visited):
+    if row < 0 or col < 0 or row >= len(matrix) or col >= len(matrix[0]):
+        return
     if visited[row][col]:
         return
 
     if matrix[row][col] != parent:
         return
 
+
+    visited[row][col] = True
+    dfs(parent, row - 1, col, matrix, visited)
+    dfs(parent, row + 1, col, matrix, visited)
+    dfs(parent, row, col - 1, matrix, visited)
+    dfs(parent, row, col + 1, matrix, visited)
 
 
 rows = int(input())
@@ -16,14 +24,18 @@ visited = []
 for _ in range(rows):
     matrix.append(list(input()))
     visited.append([False] * cols)
-
-
-
-
-
+areas = {}
 for row in range(rows):
     for col in range(cols):
         if visited[row][col]:
             continue
         key = matrix[row][col]
+
         dfs(key, row, col, matrix, visited)
+
+        if key not in areas:
+            areas[key] = 1
+        else:
+            areas[key] += 1
+
+print(areas)
