5e63aeed4c18e3e63d559e290acd9f0fb6afd792
Dragomir Petkov <dramppet@abv.bg>
2022-08-15 09:23:24 +0300

    add

---
+++ Algorithmic_python/P03IntroductionToGraph/ex/P00.py
@@ -0,0 +1,12 @@
+rows = int(input())
+cols = int(input())
+
+matrix = []
+
+for _ in range(rows):
+    line_str = input()
+    matrix.append(line_str)
+
+for row in range(rows):
+    for col in range(cols):
+        pass
