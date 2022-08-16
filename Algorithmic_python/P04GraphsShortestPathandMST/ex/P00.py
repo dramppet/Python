3fcdbb60bade5d5a6a0e38d754b0ee94f9ad42f5
Dragomir Petkov <dramppet@abv.bg>
2022-08-16 11:15:24 +0300

    add

--- Algorithmic_python/P04GraphsShortestPathandMST/ex/P00.py
+++ Algorithmic_python/P04GraphsShortestPathandMST/ex/P00.py
@@ -1,3 +1,5 @@
+from queue import PriorityQueue
+
 class Edges:
     def __init__(self, source, destination, weight):
         self.source = source
@@ -19,6 +21,34 @@ for _ in range(edges):
 
 start = int(input())
 target = int(input())
+
+max_node = max(graph.keys())
+distance = [float('inf')] * (max_node + 1)
+parent = [0] * (max_node + 1)
+
+distance[start] = 0
+pq = PriorityQueue()
+pq.put((0, start))
+
+while pq.empty():
+    min_distance, node = pq.get()
+    if node == target:
+        break
+
+    for edge in graph[node]:
+        if distance[edge] == float('inf'):
+            new_distance = min_distance + edge.weight
+
+            if new_distance < distance[edge.destination]:
+                distance[edge.destination] = new_distance
+                parent[edge.destination] = node
+                pq.put((new_distance, edge.destination))
+
+
+
+
+
+
 
 
 
