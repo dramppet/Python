f86a07511c5fb635db256486c11b3754c9e9219b
Dragomir Petkov <dramppet@abv.bg>
2022-08-16 18:20:02 +0300

    add

--- Algorithmic_python/P04GraphsShortestPathandMST/lab/P00.py
+++ Algorithmic_python/P04GraphsShortestPathandMST/lab/P00.py
@@ -1,28 +1,28 @@
-def find_root (parent, node):
+def find_root(parent, node):
     while node != parent[node]:
         node = parent[node]
     return node
 
-class Edge:
-    def __init__(self, first, second, weigh):
+class Edges:
+    def __init__(self, first, second, weight):
         self.first = first
         self.second = second
-        self.weigh = weigh
+        self.weight = weight
+
 
 edges = int(input())
-
 graph = []
 
 max_node = float('-inf')
 for _ in range(edges):
     first, second, weight = [int(x) for x in input().split(', ')]
-    graph.append(Edge(first, second, weight))
+    graph.append(Edges(first, second, weight))
     max_node = max(first, second, max_node)
 
-
 parent = [num for num in range(max_node + 1)]
-forest = []
-for edge in sorted(graph, key =lambda e : e.weight):
+forest= []
+
+for edge in sorted(graph, key = lambda e : e.weight):
     first_node_root = find_root(parent, edge.first)
     second_node_root = find_root(parent, edge.second)
 
@@ -30,3 +30,5 @@ for edge in sorted(graph, key =lambda e : e.weight):
         parent[first_node_root] = second_node_root
         forest.append(edge)
 
+for edge in forest:
+    print(f'{edge.first} - {edge.second}')
