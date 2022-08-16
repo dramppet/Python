5120e439a50f6286ff420ff5c0590ff395ffca27
Dragomir Petkov <dramppet@abv.bg>
2022-08-16 17:51:39 +0300

    add

--- Algorithmic_python/P04GraphsShortestPathandMST/lab/P00.py
+++ Algorithmic_python/P04GraphsShortestPathandMST/lab/P00.py
@@ -1,42 +1,27 @@
-from collections import deque
-nodes = int(input())
+def find_root (parent, node):
+    while node != parent[node]:
+
+
+class Edge:
+    def __init__(self, first, second, weigh):
+        self.first = first
+        self.second = second
+        self.weigh = weigh
+
 edges = int(input())
 
 graph = []
-[graph.append([]) for _ in range(nodes + 1)]
 
+max_node = float('-inf')
 for _ in range(edges):
-    source, destination = [int(x) for x in input().split()]
-    graph[source].append(destination)
+    first, second, weight = [int(x) for x in input().split(', ')]
+    graph.append(Edge(first, second, weight))
+    max_node = max(first, second, max_node)
 
-stat_node = int(input())
-destination_node = int(input())
 
-visited = [False] * (nodes + 1)
-parent = [None] * (nodes + 1)
+parent = [num for num in range(max_node + 1)]
 
-visited[stat_node] = True
-queue = deque([stat_node])
+for edge in sorted(graph, key =lambda e : e.weight):
+    first_node_root = find_root(parent, edge.first)
+    second_node_root = find_root(parent, edge.second)
 
-while queue:
-    nodess = queue.popleft()
-
-    if nodess == destination_node:
-        break
-
-    for child in graph[nodess]:
-        if visited[child]:
-            continue
-        visited[child] = True
-        queue.append(child)
-        parent[child] = nodess
-
-path = deque()
-nodess = destination_node
-
-while nodess is not None:
-    path.appendleft(nodess)
-    nodess = parent[nodess]
-
-print(len(path))
-print(*path,sep =' ')
