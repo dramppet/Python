8c580b242039b8de4ec8ff58adbd50016ed98fc5
Dragomir Petkov <dramppet@abv.bg>
2022-08-17 14:19:29 +0300

    add

--- Pyhon_Fundamntals/Dictionarys/lab/P04Odd_Occuwences.py
+++ Pyhon_Fundamntals/Dictionarys/lab/P04Odd_Occuwences.py
@@ -1,1 +1,13 @@
 data = input().split()
+words = {}
+
+for word in data:
+    word_lower = word.lower()
+
+    if word_lower not in words:
+        words[word_lower] = 0
+    words[word_lower] += 1
+
+for key, value in words.items():
+    if value % 2 != 0:
+        print(key, end=' ')
