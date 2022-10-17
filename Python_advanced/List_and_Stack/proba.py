d20d83037c60e15c6d81e1e96dfa428ea6dbf576
Dragomir Petkov <dramppet@abv.bg>
2022-10-17 21:26:08 +0300

    add

--- Python_advanced/List_and_Stack/proba.py
+++ Python_advanced/List_and_Stack/proba.py
@@ -1,13 +1,3 @@
-nums = []
+words = input()
 
-nums.append(5)
-nums.append(6)
-nums.insert(0,4)
-
-print(nums)
-print(nums[1])
-
-nums.pop()
-
-for num in nums:
-    print(num,end=', ')
+print(words[::-1])
