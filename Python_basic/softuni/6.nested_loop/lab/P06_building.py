ac64027893319d6b3c415d377fd834a9b0af3596
Dragomir Petkov <dramppet@abv.bg>
2022-11-08 11:54:09 +0200

    add

--- Python_basic/softuni/6.nested_loop/lab/P06_building.py
+++ Python_basic/softuni/6.nested_loop/lab/P06_building.py
@@ -1,12 +1,12 @@
-floor = int(input())
-apartment = int(input())
+floors = int(input())
+rooms = int(input())
 
-for i in range(floor,0,-1):
-    for j in range(apartment):
-        if i == floor:
-            print(f'L{i}{j}',end=' ')
-        elif i % 2 != 0:
-            print(f'A{i}{j}', end=' ')
+for flor in range(floors, 0, -1):
+    for room in range(rooms):
+        if flor == floors:
+            print(f'L{flor}{room}', end=' ')
+        elif flor % 2 != 0:
+            print(f'A{flor}{room}', end=' ')
         else:
-            print(f'O{i}{j}', end=' ')
+            print(f'O{flor}{room}', end=' ')
     print()
