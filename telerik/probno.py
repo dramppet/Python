ba2f4b9a4aadcf3ed6f0595a812ff47537a5ebae
Dragomir Petkov <dramppet@abv.bg>
2022-11-23 12:44:56 +0200

    add

--- telerik/probno.py
+++ telerik/probno.py
@@ -1,17 +1,4 @@
-import sys
-
-max_H = 0
-index = 0
-
-while True:
-
-    for i in range(8):
-        mounth_h = int(input())
-
-        if mounth_h > max_H:
-            max_H = mounth_h
-            index = i
-    max_H = 0
-
-    print(index)
-
+for x in range(3):
+    for y in range(3):
+        print(x + y,end=' ')
+    print('------')
