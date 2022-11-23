5aefe7e3d3c5d6ac4af2f5d4d94881156870ac77
Dragomir Petkov <dramppet@abv.bg>
2022-11-23 12:44:36 +0200

    add

--- Pyhon_Fundamntals/4.functions/lab/P01_absolute_values.py
+++ Pyhon_Fundamntals/4.functions/lab/P01_absolute_values.py
@@ -1,4 +1,16 @@
-num = [float(el) for el in input().split()]
-num_int = [abs(el) for el in num]
-print(num_int)
+# num = [float(el) for el in input().split()]
+# num_int = [abs(el) for el in num]
+# print(num_int)
+#
+
+number = 50
+if number > 10:
+    print("Larger than 10")
+if number > 20:
+    print("Larger than 20")
+if number > 50:
+    print("Larger than 50")
+else:
+    print("Not larger than 50")
+
 
