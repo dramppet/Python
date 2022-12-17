f7b1150671e1205aa9e77511db498d80185d17e9
Dragomir Petkov <dramppet@abv.bg>
2022-12-17 21:20:08 +0200

    add

--- Pyhon_Fundamntals/3.list_basics/lab/P04_search.py
+++ Pyhon_Fundamntals/3.list_basics/lab/P04_search.py
@@ -1,12 +1,12 @@
 count = int(input())
-searched_wor = input()
+searched_word = input()
 strings = list()
 fil_strings = list()
 
 for _ in range(count):
     current_strings = input()
     strings.append(current_strings)
-    if searched_wor in current_strings:
+    if searched_word in current_strings:
         fil_strings.append(current_strings)
 
 print(strings)
