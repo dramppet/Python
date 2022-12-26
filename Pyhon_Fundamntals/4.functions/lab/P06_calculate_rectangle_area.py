126c9cd5a84ed94c484b8f8c7b98f4239108f421
Dragomir Petkov <dramppet@abv.bg>
2022-12-26 16:48:33 +0200

    add

---
+++ Pyhon_Fundamntals/4.functions/lab/P06_calculate_rectangle_area.py
@@ -0,0 +1,8 @@
+def calculate_rectangle_area(w, h):
+    return w * h
+
+
+width_input = int(input())
+height_input = int(input())
+
+print(calculate_rectangle_area(width_input,height_input))
---
+++ Pyhon_Fundamntals/4.functions/lab/P07_rounding.py
@@ -0,0 +1,3 @@
+# print(list(map(lambda x:round(float(x)),input().split(' '))))
+
+print([round(float(x)) for x in input().split(' ')])
