7ceb2b0ed1309a1d73d00c888df6e41eebb3248e
Dragomir Petkov <dramppet@abv.bg>
2022-12-30 11:58:05 +0200

    add

---
+++ Pyhon_Fundamntals/6.object_and_classes/lab/P03_Email.py
@@ -0,0 +1,10 @@
+class Email:
+
+    def __init__(self, sender, receiver, content):
+        self.sender = sender
+        self.receiver = receiver
+        self.content = content
+        self.is_sent = False
+
+    def send(self):
+        self.is_sent = True
