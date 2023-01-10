df6a3a0936d3688a95b2f4de8970ac05ccee35f9
Dragomir Petkov <dramppet@abv.bg>
2023-01-10 20:05:34 +0200

    add

--- Python_advanced/List_and_Stack/lab/P02_MatchingParentheses.py
+++ Python_advanced/List_and_Stack/lab/P02_MatchingParentheses.py
@@ -1,10 +1,19 @@
-exp = input()
+expression =  input()
 
 stack = []
 
-for idx in range(len(exp)):
-    if exp[idx] == '(':
+# for el in range(len(expression)):
+#     if expression[el] == '(':
+#         stack.append(el)
+#     elif expression[el] == ')':
+#         start = stack.pop()
+#         end = el
+#         print(expression[start:end + 1])
+
+for idx, ch in enumerate(expression):
+    if ch == '(':
         stack.append(idx)
-    elif exp[idx] == ')':
-        start_idx = stack.pop()
-        print(exp[start_idx: idx + 1])
+    elif ch == ')':
+        start = stack.pop()
+        finish = idx
+        print(expression[start:finish + 1])
