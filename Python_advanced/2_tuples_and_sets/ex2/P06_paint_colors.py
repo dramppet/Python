faa48dc21cb874a9685d663f7db22251ef328124
Dragomir Petkov <dramppet@abv.bg>
2023-01-17 20:59:49 +0200

    add

--- Python_advanced/2_tuples_and_sets/ex2/P06_paint_colors.py
+++ Python_advanced/2_tuples_and_sets/ex2/P06_paint_colors.py
@@ -15,7 +15,7 @@ while words:
     first_word = words.popleft()
     second_word = words.pop() if words else ''
 
-    for color in (first_word + second_word, second_word + words):
+    for color in (first_word + second_word, second_word + first_word):
         if color in colors:
             result.append(color)
             break
@@ -23,3 +23,9 @@ while words:
         for el in (first_word[::-1], second_word[::-1]):
             if el:
                 words.insert(len(words) // 2 ,el)
+
+for color in set(red_colors.keys()).intersection(result):
+    if not red_colors[color].issubset(result):
+        result.remove(color)
+
+print(result)
