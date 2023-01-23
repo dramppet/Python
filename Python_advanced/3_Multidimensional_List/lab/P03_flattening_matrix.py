2e9dc6e995b6d092c20ae926bfadcf3f7729f250
Dragomir Petkov <dramppet@abv.bg>
2023-01-23 22:10:26 +0200

    add

--- Python_advanced/3_Multidimensional_List/lab/P03_flattening_matrix.py
+++ Python_advanced/3_Multidimensional_List/lab/P03_flattening_matrix.py
@@ -1,5 +1,11 @@
-rows = int(input())
+count_rows = int(input())
 
-matrix = [ int(x) for x in input().split(', ') for _ in range(rows)]
-flatt = [num for row in matrix for  num in row]
-print(flatt)
+matrix = [[int(el) for el in input().split(", ")]for _ in range(count_rows)]
+matrix_comp = [num for row in matrix for num in row]
+
+m = []
+for row in matrix:
+    for col in row:
+        m.append(col)
+
+print(matrix_comp)
