04a0913648bf0fc8196bf325cc8e65334c0bd228
Dragomir Petkov <dramppet@abv.bg>
2023-01-23 21:48:40 +0200

    add

--- Python_advanced/3_Multidimensional_List/lab/P02_evan_matrix.py
+++ Python_advanced/3_Multidimensional_List/lab/P02_evan_matrix.py
@@ -1,14 +1,9 @@
-rows = int(input())
+count_row = int(input())
 
-# result = []
-#
-# for _ in range(rows):
-#     row = [int(x) for x in input().split(', ')]
-#     result.append([el for el in row if el % 2 == 0])
-#
-# print(result)
+matrix = []
 
-# comprehension
+for _ in range(count_row):
+    row = [int(x) for x in input().split(', ')]
+    matrix.append([el for el in row if el % 2 == 0])
 
-# matrix = [[int(x) for x in input().split(', ')] for _ in range(rows)]
-# print([x for x in row if x % 2 == 0] for row in matrix)
+print(matrix)
