91c021b70fa9778e46764a5d15aaefe17faf9a5c
Dragomir Petkov <dramppet@abv.bg>
2023-01-25 22:36:11 +0200

    add

---
+++ Python_advanced/4_Functions_Advanced/lab/P04_rectangle.py
@@ -0,0 +1,13 @@
+def rectangle(length, width):
+    def rectangle_area():
+        return length  * width
+    def rectangle_perimeter():
+        return 2 * (length + width)
+
+    if type(length) != int or type(width) != int:
+        return "Enter valid values!"
+
+    return f'''Rectangle area: {rectangle_area()}
+               Rectangle perimeter: {rectangle_perimeter()} '''
+
+print(rectangle(2, 10))
