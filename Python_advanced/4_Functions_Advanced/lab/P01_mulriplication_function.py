dba14b1f71a21c52324f8052a6d985d8327f14ee
Dragomir Petkov <dramppet@abv.bg>
2023-01-25 21:20:04 +0200

    add

--- Python_advanced/4_Functions_Advanced/lab/P01_mulriplication_function.py
+++ Python_advanced/4_Functions_Advanced/lab/P01_mulriplication_function.py
@@ -1,8 +1,14 @@
+# def multiply(*args):
+#     product = 1
+#     for num in args:
+#         product *= num
+#     return product
+from functools import reduce
+
+
 def multiply(*args):
-    product = 1
-    for num in args:
-        product *= num
-    return product
+    return reduce(lambda x, y: x * y,args)
+
 
 print(multiply(1, 4, 5))
 print(multiply(4, 5, 6, 1, 3))
