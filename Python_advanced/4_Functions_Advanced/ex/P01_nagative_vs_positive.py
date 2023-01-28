0c89c5e3f39d5239b4aa796af71118804a1d90e1
Dragomir Petkov <dramppet@abv.bg>
2023-01-28 10:34:35 +0200

    add

--- Python_advanced/4_Functions_Advanced/ex/P01_nagative_vs_positive.py
+++ Python_advanced/4_Functions_Advanced/ex/P01_nagative_vs_positive.py
@@ -1,2 +1,18 @@
-def multiply(*args):
-    pass
+numbers = list(map(int, input().split()))
+
+sum_positive = 0
+sum_negative = 0
+
+for num in numbers:
+    if num > 0:
+        sum_positive += num
+    else:
+        sum_negative += num
+
+print(sum_negative)
+print(sum_positive)
+
+if abs(sum_negative) > sum_positive:
+    print('The negatives are stronger than the positives')
+else:
+    print('The positives are stronger than the negatives')
