70681249048b85acb22df1f5f7bb84327765399d
Dragomir Petkov <dramppet@abv.bg>
2023-01-28 10:01:40 +0200

    add

--- Python_advanced/4_Functions_Advanced/lab/P04_rectangle.py
+++ Python_advanced/4_Functions_Advanced/lab/P04_rectangle.py
@@ -8,6 +8,6 @@ def rectangle(length, width):
         return "Enter valid values!"
 
     return f'''Rectangle area: {rectangle_area()}
-               Rectangle perimeter: {rectangle_perimeter()} '''
+               Rectangle perimeter: {rectangle_perimeter()}'''
 
 print(rectangle(2, 10))
