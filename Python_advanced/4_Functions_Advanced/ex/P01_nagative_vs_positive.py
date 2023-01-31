07e14b25bf7624e334c932bef0b0b94140089a97
Dragomir Petkov <dramppet@abv.bg>
2023-01-31 18:06:46 +0200

    add

--- Python_advanced/4_Functions_Advanced/ex/P01_nagative_vs_positive.py
+++ Python_advanced/4_Functions_Advanced/ex/P01_nagative_vs_positive.py
@@ -1,13 +1,16 @@
 numbers = list(map(int, input().split()))
 
-sum_positive = 0
-sum_negative = 0
+# sum_positive = 0
+# sum_negative = 0
+#
+# for num in numbers:
+#     if num > 0:
+#         sum_positive += num
+#     else:
+#         sum_negative += num
 
-for num in numbers:
-    if num > 0:
-        sum_positive += num
-    else:
-        sum_negative += num
+sum_positive = sum( el for el in numbers if el > 0)
+sum_negative = sum( el for el in numbers if el < 0)
 
 print(sum_negative)
 print(sum_positive)
