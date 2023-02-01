4f96286d3c095379d095ad9dc7116a1a4dbe8c29
Dragomir Petkov <dramppet@abv.bg>
2023-02-01 18:54:40 +0200

    add

--- Python_advanced/3_Multidimensional_List/lab/P01_sum_matrix_elements.py
+++ Python_advanced/3_Multidimensional_List/lab/P01_sum_matrix_elements.py
@@ -1,4 +1,20 @@
 rows, columns = map(int, input().split(", "))
 
-print(rows)
-print(columns)
+matrix = []
+
+sum_row_matrix = 0
+
+for _ in range(rows):
+    row = [int(x) for x in input().split(", ")]
+    sum_row_matrix += sum(row)
+    matrix.append(row)
+
+sum_matrix = 0
+
+for row_idx in range(len(matrix)):
+    row_matrix = matrix[row_idx]
+    sum_matrix += sum(row_matrix)
+
+print(sum_matrix)
+print(sum_row_matrix)
+print(matrix)
