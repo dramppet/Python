a0d7b05f48fba4e0fabefaf5b94f0342904daca2
Dragomir Petkov <dramppet@abv.bg>
2023-02-02 20:55:19 +0200

    add

--- Python_advanced/3_Multidimensional_List/lab/P05_primary_diagonal.py
+++ Python_advanced/3_Multidimensional_List/lab/P05_primary_diagonal.py
@@ -4,10 +4,14 @@ matrix = []
 for _ in range(rows):
     matrix.append([int(el) for el in input().split()])
 
+print(len(matrix))
+
 primary_diagonal = 0
+secondary_diagonal = 0
 
 for row_idx in range(len(matrix)):
     row = row_idx
     primary_diagonal += matrix[row_idx][row_idx]
+    secondary_diagonal += matrix[len(row_idx) - 1 ][row_idx - 1]
 
 print(primary_diagonal)
