c18497c43284f3fa4192d6ac053c6824998f45cf
Dragomir Petkov <dramppet@abv.bg>
2023-02-02 20:55:05 +0200

    add

--- Python_advanced/3_Multidimensional_List/lab/P_05_01_secondary_diagonal.py
+++ Python_advanced/3_Multidimensional_List/lab/P_05_01_secondary_diagonal.py
@@ -3,4 +3,9 @@ rows = int(input())
 matrix = [[int(el) for el in input().split()]for _ in range(rows)]
 
 
-sum_
+sum_secondary_diagonal = 0
+
+for i in range(len(matrix) - 1, -1, -1):
+    sum_secondary_diagonal += matrix[i][(len(matrix) - 1) - i]
+
+print(sum_secondary_diagonal)
