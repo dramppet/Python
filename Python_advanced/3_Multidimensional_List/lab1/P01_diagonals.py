9ecfc6e44e2064b1021866891ccf12b68b593f7f
Dragomir Petkov <dramppet@abv.bg>
2023-02-09 18:18:08 +0200

    add

--- Python_advanced/3_Multidimensional_List/lab1/P01_diagonals.py
+++ Python_advanced/3_Multidimensional_List/lab1/P01_diagonals.py
@@ -6,4 +6,5 @@ primary_diagonal = [matrix[row][row] for row in range(len(matrix))]
 
 secondary_diagonal =[matrix[row][len(matrix) - row - 1] for row in range(len(matrix) - 1, - 1, -1)]
 
-print()
+print(f"Primary diagonal: {', '.join(str(x) for x in primary_diagonal)}. Sum: {sum(primary_diagonal)}")
+print(f"Secondary diagonal: {', '.join([str(x) for x in secondary_diagonal][::-1])}. Sum: {sum(secondary_diagonal)}")
