aea65ece05a86b849067c5800ab8cfa1ac3f0f2b
Dragomir Petkov <dramppet@abv.bg>
2023-02-09 18:15:51 +0200

    add

--- Python_advanced/3_Multidimensional_List/lab1/P01_diagonals.py
+++ Python_advanced/3_Multidimensional_List/lab1/P01_diagonals.py
@@ -4,3 +4,6 @@ matrix = [ [int(el) for el in input().split(", ")]for _ in range(rows)]
 
 primary_diagonal = [matrix[row][row] for row in range(len(matrix))]
 
+secondary_diagonal =[matrix[row][len(matrix) - row - 1] for row in range(len(matrix) - 1, - 1, -1)]
+
+print()
