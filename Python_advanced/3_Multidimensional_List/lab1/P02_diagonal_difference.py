5808abd373a1642f492069c4f23b351b3a445f38
Dragomir Petkov <dramppet@abv.bg>
2023-02-09 18:34:59 +0200

    add

--- Python_advanced/3_Multidimensional_List/lab1/P02_diagonal_difference.py
+++ Python_advanced/3_Multidimensional_List/lab1/P02_diagonal_difference.py
@@ -2,4 +2,10 @@ rows = int(input())
 
 matrix = [ [int(el) for el in input().split(" ")] for _ in range(rows)]
 
-print(matrix)
+sum_primary, sum_secondary = 0, 0
+
+for r in range(rows):
+    sum_primary += matrix[r][r]
+    sum_secondary += matrix[rows - r - 1][r]
+
+print(abs(sum_primary - sum_secondary))
