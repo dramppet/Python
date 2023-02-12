a12f31985f1c2b703bc2ba0cc4bbf47b57385812
Dragomir Petkov <dramppet@abv.bg>
2023-02-12 12:57:18 +0200

    add

--- Python_advanced/3_Multidimensional_List/lab1/P03_2x2_square_in_matrix.py
+++ Python_advanced/3_Multidimensional_List/lab1/P03_2x2_square_in_matrix.py
@@ -1,4 +1,17 @@
 rows, columns = list(map(int,input().split()))
-
 matrix = [ input().split() for _ in range(rows)]
+
+equal_blocks = 0
+
+for row in range(rows - 1):
+    for col in range(columns):
+        symbols = matrix[row][col]
+
+        if symbols == matrix[row][col + 1] and symbols == matrix[row + 1][col] and symbols == matrix[row][col + 1]:
+            equal_blocks += 1
+        else:
+            break
+
+print(equal_blocks)
+
 
