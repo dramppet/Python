3f867ad9eb7937e03e5a1530c3eb757a5d43fd51
Dragomir Petkov <dramppet@abv.bg>
2023-02-13 11:03:44 +0200

    add

--- Python_advanced/3_Multidimensional_List/ex2/P03_knight_game.py
+++ Python_advanced/3_Multidimensional_List/ex2/P03_knight_game.py
@@ -16,4 +16,34 @@ position = (
 remove_knight = 0
 
 while True:
-    pass
+    max_attacks = 0
+    knight_position = []
+
+    for row in range(size):
+        for col in range(size):
+            if matrix[row][col] == 'K':
+                attacks = 0
+
+                for pos in position:
+                    post_row = row + pos[0]
+                    post_col = col + pos[1]
+
+                    if  0 <= post_row < size and 0 <= post_col < size:
+                        if matrix[post_row][post_col] == "K":
+                            attacks += 1
+
+                if attacks > max_attacks:
+                    knight_position = [row, col]
+                    max_attacks = attacks
+
+    if knight_position:
+        matrix[knight_position[0]][knight_position[1]] = "0"
+        remove_knight += 1
+    else:
+        break
+
+print(remove_knight)
+
+
+
+
