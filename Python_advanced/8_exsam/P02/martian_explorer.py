1bc0bb2ef7a34b5f250744d025704cf99e4e14d7
Dragomir Petkov <dramppet@abv.bg>
2023-02-15 12:34:24 +0200

    add

--- Python_advanced/8_exsam/P02/martian_explorer.py
+++ Python_advanced/8_exsam/P02/martian_explorer.py
@@ -2,14 +2,16 @@ def move_rover(direction, row, col):
     if direction == "left":
         return row, col - 1
     if direction == "right":
-        return row,col + 1
+        return row, col + 1
     if direction == "up":
-        return row - 1,col
-    if direction =="down":
-        return row + 1,col
+        return row - 1, col
+    if direction == "down":
+        return row + 1, col
 
-def out_side(rover_row, rover_col, SIZE):
-        pass
+
+def out_side(row, col, SIZE):
+    return row < 0 or col < 0 or row >= SIZE or col >= SIZE
+
 
 SIZE = 6
 
@@ -24,7 +26,6 @@ for row in range(SIZE):
     area.append(row_elements)
 
 directions = input().split(", ")
-
 
 for direction in directions:
     rover_row, rover_col = move_rover(direction, rover_row, rover_col)
