f80d0e9ea1e358c17df089a98093e19963e6302c
Dragomir Petkov <dramppet@abv.bg>
2023-02-15 12:29:52 +0200

    add

--- Python_advanced/8_exsam/P02/martian_explorer.py
+++ Python_advanced/8_exsam/P02/martian_explorer.py
@@ -1,6 +1,30 @@
+def move_rover(direction, row, col):
+    if direction == "left":
+        return row, col - 1
+    elif direction == "right":
+        return row,col + 1
+    elif direction == "up":
+        return row - 1,col
+    elif direction =="down":
+        return row + 1,col
+
 SIZE = 6
 
-rover_row, rover_col = 0
+rover_row, rover_col = 0, 0
 area = []
 
 for row in range(SIZE):
+    row_elements = input().split()
+    for col in range(SIZE):
+        if row_elements[col] == "E":
+            rover_row, rover_col = row, col
+    area.append(row_elements)
+
+directions = input().split(", ")
+
+
+
+
+
+for direction in directions:
+    rover_row, rover_col = move_rover(direction, rover_row, rover_col)
