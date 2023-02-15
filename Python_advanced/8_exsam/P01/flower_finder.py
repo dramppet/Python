f8d5b5a7d51159ea091d47fbc3db23096611972b
Dragomir Petkov <dramppet@abv.bg>
2023-02-15 11:09:09 +0200

    add

--- Python_advanced/8_exsam/P01/flower_finder.py
+++ Python_advanced/8_exsam/P01/flower_finder.py
@@ -1,3 +1,12 @@
 from collections import deque
 
+rose = {letter:False for letter in "rose"}
+tulip = {letter:False for letter in "tulip"}
+lotus = {letter:False for letter in "lotus"}
+daffodil = {letter:False for letter in "daffodil"}
+
 vowels, consonants = deque(input().split()), deque(input().split())
+
+while vowels and consonants:
+    vowel = vowels.popleft()
+    consonant = consonants.pop()
