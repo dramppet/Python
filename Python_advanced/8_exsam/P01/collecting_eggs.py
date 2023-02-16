b43108451816c7d664b73e44e8187df58c243bdb
Dragomir Petkov <dramppet@abv.bg>
2023-02-16 09:43:29 +0200

    add

--- Python_advanced/8_exsam/P01/collecting_eggs.py
+++ Python_advanced/8_exsam/P01/collecting_eggs.py
@@ -2,41 +2,35 @@ from collections import deque
 
 SIZE_BOX = 50
 eggs = deque([int(el) for el in input().split(", ")])
-piece_of_paper = deque([int(el) for el in input().split(", ")])
+papers = deque([int(el) for el in input().split(", ")])
 
 egg_box = 0
-box = 0
+boxes = 0
 full_box = False
 
-while True:
-    if len(eggs) == 0:
-        break
-    if len(piece_of_paper)  == 0:
-        break
+while eggs and papers:
     egg = eggs.popleft()
     if egg <= 0:
         continue
     if egg == 13:
-        a = piece_of_paper.popleft()
-        b = piece_of_paper.pop()
-
-        a,b = piece_of_paper.append(a),piece_of_paper.appendleft(b)
-    paper = piece_of_paper.pop()
+        papers[0], papers[-1] = papers[-1],papers[0]
+        continue
+    paper = papers.pop()
 
     product = egg + paper
     if product + egg_box <= SIZE_BOX:
         egg_box = product
         full_box = True
-        box += 1
+        boxes += 1
     else:
         egg_box = 0
         continue
 
 if full_box:
-    print(f"Great! You filled {box} boxes.")
+    print(f"Great! You filled {boxes} boxes.")
 else:
     print("Sorry! You couldn't fill any boxes!")
 if eggs:
     print(f"Eggs left: {', '.join([str(x) for x in eggs])}")
-if piece_of_paper:
-    print(f"Pieces of paper left: {', '.join([str(x) for x in piece_of_paper])}")
+if papers:
+    print(f"Pieces of paper left: {', '.join([str(x) for x in papers])}")
