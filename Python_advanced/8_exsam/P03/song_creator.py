9a0795428f3f4e6f7d96de77aa2c323b4883b111
Dragomir Petkov <dramppet@abv.bg>
2023-02-16 11:15:33 +0200

    add

--- Python_advanced/8_exsam/P03/song_creator.py
+++ Python_advanced/8_exsam/P03/song_creator.py
@@ -1,7 +1,19 @@
 def add_songs(*tuples):
+    songs = {}
+    output = []
 
-    for t,a in tuples:
+    for t in tuples:
         song, lyrics = t
+
+        if song not in songs:
+            songs[song] = []
+        songs[song].extend(lyrics)
+
+    for s_title, s_lyrics in songs.items():
+        output.append("- " + s_title)
+        output.extend(s_lyrics)
+
+    return
 
 
 
