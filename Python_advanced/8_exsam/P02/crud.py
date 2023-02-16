2285b7e04d6ee6015109dcc5a7ac357417e4331f
Dragomir Petkov <dramppet@abv.bg>
2023-02-16 09:26:19 +0200

    add

--- Python_advanced/8_exsam/P02/crud.py
+++ Python_advanced/8_exsam/P02/crud.py
@@ -1,9 +1,28 @@
 SIZE = 6
 
 matrix = []
+coordinates = [0, 0]
 
 for row in range(SIZE):
     line = list(input().split())
     matrix.append(line)
 
-print(matrix)
+coordinates = *a
+
+row = int(coordinates[0])
+col = int(coordinates[1])
+
+while True:
+    command = input().split(", ")
+
+    if command[0] == "Stop":
+        break
+
+    if command[0] == "Create":
+        pass
+    elif command[0] == "Update":
+        pass
+    elif command[0] == "Delete":
+        pass
+    elif command[0] == "Read":
+        pass
