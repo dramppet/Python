62ced7a286eed969b8a9b542bd3a7c439966a4cb
Dragomir Petkov <dramppet@abv.bg>
2023-02-17 14:53:55 +0200

    add

--- Python_advanced/8_exsam/P02/pawn_wars.py
+++ Python_advanced/8_exsam/P02/pawn_wars.py
@@ -1,5 +1,13 @@
-ROWAS_COUNT = 8
+def find_player_position(matrix, player):
+    pass
 
-matrix = [ input().split(' ') for _ in range(ROWAS_COUNT)]
 
-print(matrix)
+ROWS_COUNT = 8
+COLUMNS_COUNT = 8
+
+matrix = [input().split(' ') for _ in range(ROWS_COUNT)]
+
+current_player, other_player = find_player_position(matrix, 'w'), find_player_position(matrix, 'b')
+
+current_delta = -1
+other_delta = + 1
