c6b4c200a4983c5c231bd132358df531420ff676
Dragomir Petkov <dramppet@abv.bg>
2023-02-17 13:06:35 +0200

    add

--- Python_advanced/8_exsam/P01/cristmas_elves.py
+++ Python_advanced/8_exsam/P01/cristmas_elves.py
@@ -2,32 +2,33 @@ from collections import deque
 
 elf_energies, boxes = deque(list(map(int, input().split()))), deque(list(map(int, input().split())))
 
-turns_count = 1
+turns_count = 0
 total_energy_spent = 0
 toys_count = 0
 
 while boxes and elf_energies:
-    box = boxes.pop()
-    elf_energy = elf_energies.popleft()
-
-    while elf_energy and elf_energies[0] < 5:
+    while elf_energies and elf_energies[0] < 5:
         elf_energies.popleft()
 
     if not elf_energies:
         break
+
+    toys_count += 1
+    box = boxes.pop()
+    elf_energy = elf_energies.popleft()
 
     if turns_count % 15 == 0 and (2 * box) <= elf_energy:
         total_energy_spent += 2 * box
         elf_energies.append(elf_energy - (2 * box))
     elif turns_count % 5 == 0 and box <= elf_energy:
         total_energy_spent +=  box
-        elf_energies.append(elf_energy - box + 1)
+        elf_energies.append(elf_energy - box)
     elif turns_count % 3 == 0 and (2 * box) <= elf_energy:
         toys_count += 2
         total_energy_spent += 2 * box
         elf_energies.append(elf_energy - (2 * box) + 1)
     elif box <= elf_energy:
-        turns_count += 1
+        toys_count += 1
         total_energy_spent += 1 * box
         elf_energies.append(elf_energy - box + 1 )
     else:
@@ -36,3 +37,9 @@ while boxes and elf_energies:
 
 print(f"Toys: {toys_count}")
 print(f"Energy: {total_energy_spent}")
+
+if elf_energies:
+    print(f"Elves left: {', '.join([str(x) for x in elf_energies])}")
+
+if boxes:
+    print(f"Boxes left: {', '.join([str(x) for x in boxes])}")
