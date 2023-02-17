342ca468ecd901bc23ddd85767541b1f8ad2b5e6
Dragomir Petkov <dramppet@abv.bg>
2023-02-17 11:52:52 +0200

    add

--- Python_advanced/8_exsam/P01/cristmas_elves.py
+++ Python_advanced/8_exsam/P01/cristmas_elves.py
@@ -1,3 +1,13 @@
 from collections import deque
 
-energy , materials_box  = deque(list(map(int,input().split()))), deque(list(map(int,input().split())))
+elf_energies, boxes = deque(list(map(int, input().split()))), deque(list(map(int, input().split())))
+
+turns_count = 0
+total_energy_spent = 0
+total_boxes = 0
+
+while boxes and elf_energies:
+    box = boxes.pop()
+    elf_energy = elf_energies.popleft()
+
+    if turns_count % 15 == 0:
