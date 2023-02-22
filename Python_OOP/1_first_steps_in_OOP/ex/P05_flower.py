9c90738efc46fd6bcc66cff6b722d0f2d527db0f
Dragomir Petkov <dramppet@abv.bg>
2023-02-22 21:49:19 +0200

    add

--- Python_OOP/1_first_steps_in_OOP/ex/P05_flower.py
+++ Python_OOP/1_first_steps_in_OOP/ex/P05_flower.py
@@ -1,2 +1,15 @@
 class Flower:
-    pass
+    def __init__(self, name, water_requirements):
+        self.name = name
+        self.water_requirements = water_requirements
+
+
+
+
+flower = Flower("Lilly", 100)
+flower.water(50)
+print(flower.status())
+flower.water(60)
+print(flower.status())
+flower.water(100)
+print(flower.status())
