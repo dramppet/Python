40f023ad23fcc8ac9b2a495b8355b098420ea3f0
Dragomir Petkov <dramppet@abv.bg>
2023-02-22 21:49:51 +0200

    add

--- Python_OOP/1_first_steps_in_OOP/ex/P05_flower.py
+++ Python_OOP/1_first_steps_in_OOP/ex/P05_flower.py
@@ -2,6 +2,9 @@ class Flower:
     def __init__(self, name, water_requirements):
         self.name = name
         self.water_requirements = water_requirements
+        self.is_happy = False
+
+
 
 
 
