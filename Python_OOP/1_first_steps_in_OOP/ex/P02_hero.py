d26e674c1516305b1328097b5b89732c6e649a7e
Dragomir Petkov <dramppet@abv.bg>
2023-02-23 20:12:55 +0200

    add

--- Python_OOP/1_first_steps_in_OOP/ex/P02_hero.py
+++ Python_OOP/1_first_steps_in_OOP/ex/P02_hero.py
@@ -3,7 +3,7 @@ class Hero:
         self.name = name
         self.health = health
 
-    def defend(self,damage):
+    def defend(self,damage: int):
         self.health -= damage
         if self.health <= 0:
             self.health = 0
