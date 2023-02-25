3a030a0089d8e4479c2b2456b4a88ae8532a8bae
Dragomir Petkov <dramppet@abv.bg>
2023-02-25 10:15:05 +0200

    add

--- Python_OOP/2_classes_and_objects/lab/P02_point.py
+++ Python_OOP/2_classes_and_objects/lab/P02_point.py
@@ -3,14 +3,14 @@ class Point:
         self.x = x
         self.y = y
 
-    def __str__(self):
-        return f"The point has coordinates ({self.x},{self.y})"
-
     def set_x(self, new_x):
         self.x = new_x
 
     def set_y(self, new_y):
         self.y = new_y
+
+    def __str__(self):
+        return f"The point has coordinates ({self.x},{self.y})"
 
 
 p = Point(2, 4)
@@ -18,5 +18,3 @@ print(p)
 p.set_x(3)
 p.set_y(5)
 print(p)
-
-
