7c8291d3d047dee4e6a5ac00b88c8f9c92b7fa71
Dragomir Petkov <dramppet@abv.bg>
2023-02-27 18:36:47 +0200

    add

--- Python_OOP/2_classes_and_objects/ex/P02_time.py
+++ Python_OOP/2_classes_and_objects/ex/P02_time.py
@@ -1,5 +1,24 @@
 class Time:
-    pass
+    max_hours = 23
+    max_minutes = 59
+    max_seconds = 59
+
+    def __init__(self, hours, minutes, seconds):
+        self.hours = hours
+        self.minutes = minutes
+        self.seconds = seconds
+
+    def set_time(self, hours, minutes, seconds):
+        self.hours = hours
+        self.minutes = minutes
+        self.seconds = seconds
+
+    def get_time(self):
+        return f"{self.hours:02d}:{self.minutes:02d}:{self.seconds:02d}"
+
+    def next_second(self):
+        pass
+
 
 time = Time(9, 30, 59)
 print(time.next_second())
