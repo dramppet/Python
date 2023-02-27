7542967b92fc77c6bb6fa4853cc7144cdb007cfb
Dragomir Petkov <dramppet@abv.bg>
2023-02-27 18:20:36 +0200

    add

--- Python_OOP/2_classes_and_objects/ex/P01_vet.py
+++ Python_OOP/2_classes_and_objects/ex/P01_vet.py
@@ -1,10 +1,29 @@
 class Vet:
-    ANIMALS = []
-    SPACE = 5
-    def __init__(self, name):
+    animals = []
+    space = 5
+    def __init__(self, name: str):
         self.name = name
         self.animals = []
 
+    def register_animal(self, animal_name):
+        if 0 < Vet.space < 6:
+            self.animals.append(animal_name)
+            Vet.animals.append(animal_name)
+            Vet.space -= 1
+            return f"{animal_name} registered in the clinic"
+        else:
+            return f"{animal_name} unregistered successfully"
+
+    def unregister_animal(self, animal_name):
+        if animal_name in self.animals:
+            self.animals.remove(animal_name)
+            Vet.animals.remove(animal_name)
+            return f"{animal_name} unregistered successfully"
+        else:
+            return f"{animal_name} not in the clinic"
+
+    def info(self):
+        return f"{self.name} has {len(self.animals)} animals. {self.SPACE - len(self.ANIMALS)} space left in clinic"
 
 
 peter = Vet("Peter")
