9b16784cb14be60d9e8a3e3453a720ef1b693f93
Dragomir Petkov <dramppet@abv.bg>
2023-02-28 22:44:32 +0200

    add

--- Python_OOP/3_inheritance/lab/P01_food/project/fruit.py
+++ Python_OOP/3_inheritance/lab/P01_food/project/fruit.py
@@ -1,7 +1,7 @@
-from food import Food
+from project.food import Food
 
 
 class Fruit(Food):
-    def __init__(self, name:str, expiration_data:str):
-        self.name = name
-        super().__init__(expiration_data)
+    def __init__(self, name, expiration_date ):
+        super().__init__(expiration_date)
+        self.name  = name
