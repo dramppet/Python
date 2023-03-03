5a7c5e8a0376df8def946e34d87316d48474fe26
Dragomir Petkov <dramppet@abv.bg>
2023-03-03 13:22:00 +0200

    add

--- Python_OOP/4_Encapsulation/lab/P01_person.py
+++ Python_OOP/4_Encapsulation/lab/P01_person.py
@@ -1,13 +1,13 @@
 class Person:
     def __init__(self, name, age):
-        self.name = name
-        self.age = age
+        self.__name = name
+        self.__age = age
 
     def get_name(self):
-        return self.name
+        return self.__name
 
     def get_age(self):
-        return self.age
+        return self.__age
 
 
 person = Person("George", 32)
