1d4ddcafce535c991397f38940214381c092529c
Dragomir Petkov <dramppet@abv.bg>
2023-03-03 13:32:41 +0200

    add

--- Python_OOP/4_Encapsulation/lab/P02_mammal.py
+++ Python_OOP/4_Encapsulation/lab/P02_mammal.py
@@ -1,18 +1,18 @@
 class Mammal:
-    kingdom = "animals"
-    def __init__(self, name, type_mammal, sound):
+    __kingdom = "animals"
+    def __init__(self, name, type, sound):
         self.name = name
-        self.type_mammal = type_mammal
+        self.type = type
         self.sound = sound
 
     def make_sound(self):
         return f"{self.name} makes {self.sound}"
 
     def get_kingdom(self):
-        return Mammal.kingdom
+        return self.__kingdom
 
     def info(self):
-        return f"{self.name} is of type {self.type_mammal}"
+        return f"{self.name} is of type {self.type}"
 
 
 mammal = Mammal("Dog", "Domestic", "Bark")
