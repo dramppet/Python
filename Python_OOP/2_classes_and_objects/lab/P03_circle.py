92d2afb9caeeecf47132a9c383b626f54b751af9
Dragomir Petkov <dramppet@abv.bg>
2023-03-05 19:02:39 +0200

    add

--- Python_OOP/2_classes_and_objects/lab/P03_circle.py
+++ Python_OOP/2_classes_and_objects/lab/P03_circle.py
@@ -1,9 +1,20 @@
 class Circle:
-    pass
+    pi = 3.14
+
+    def __init__(self, radius):
+        self.radius = radius
+
+    def set_radius(self, radius):
+        self.radius = radius
+
+    def get_area(self):
+        return self.pi * self.radius ** 2
+
+    def get_circumference(self):
+        return 2 * self.pi * self.radius
 
 
-
-circle = Circle(10)
-circle.set_radius(12)
+circle = Circle(15)
+circle.set_radius(160)
 print(circle.get_area())
 print(circle.get_circumference())
