d2f4a9bd9f156fd6da9b8125d3cb299bf174f3c7
Dragomir Petkov <dramppet@abv.bg>
2023-03-07 18:38:50 +0200

    add

---
+++ Python_OOP/4_static_and_class_methods/lab/P01_calculator.py
@@ -0,0 +1,18 @@
+class Calculator:
+
+    @staticmethod
+    def add(*args):
+        return sum(args)
+
+    @staticmethod
+    def multiply(*args):
+        return
+
+
+    @staticmethod
+    def divide(*args):
+        return
+
+    @staticmethod
+    def subtract(*args):
+        return
