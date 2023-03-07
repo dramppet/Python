f808dadac068e68350d843b73897eda1ba827d26
Dragomir Petkov <dramppet@abv.bg>
2023-03-07 19:24:45 +0200

    add

--- Python_OOP/4_static_and_class_methods/lab/P02_shop.py
+++ Python_OOP/4_static_and_class_methods/lab/P02_shop.py
@@ -1,10 +1,21 @@
-import unittest
+class Shop:
+    def __init__(self, name, type_shop, capacity):
+        self.name = name
+        self.type = type_shop
+        self.capacity = capacity
+        self.items = {}
 
+    @classmethod
+    def small_shop(cls, name: str, type_shop: str):
+        return cls(name, type_shop, 10)
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+    def add_item(self, item_name):
+        if sum(self.items.values()) >= self.capacity:
+            return "Not enough capacity in the shop"
 
+        if item_name not in self.items:
+            self.items[item_name] = 0
+        self.items[item_name] += 1
+        return f"{item_name} added to the shop"
 
-if __name__ == '__main__':
-    unittest.main()
+    def remove_item(self, item_name, amount):
