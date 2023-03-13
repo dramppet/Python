67ba062917fc2fa8f1f49564c3f6b7ea9b6c93cd
Dragomir Petkov <dramppet@abv.bg>
2023-03-13 18:29:29 +0200

    add

---
+++ Python_OOP/5_polymorphism_and_abstraction/ex/P01_vehicle.py
@@ -0,0 +1,47 @@
+from abc import ABC, abstractmethod
+
+
+class Vehicle(ABC):
+    def __init__(self, fuel_quantity, fuel_consumption):
+        self.fuel_quantity = fuel_quantity
+        self.fuel_consumption = fuel_consumption
+
+    @abstractmethod
+    def drive(self):
+        ...
+
+    @abstractmethod
+    def refuel(self):
+        pass
+
+
+class Car(Vehicle):
+    CONDITIONER_ON = 0.9
+
+    def __init__(self, fuel_quantity, fuel_consumption):
+        super().__init__(fuel_quantity, fuel_consumption)
+
+    def drive(self, distance):
+        consumption = (self.fuel_consumption + Car.CONDITIONER_ON) * distance
+
+        if self.fuel_quantity >= consumption:
+            self.fuel_quantity -= consumption
+
+    def refuel(self, fuel):
+        self.fuel_quantity += fuel
+
+
+class Truck(Vehicle):
+    CONDITIONER_ON = 1.6
+
+    def __init__(self, fuel_quantity, fuel_consumption):
+        super().__init__(fuel_quantity, fuel_consumption)
+
+    def drive(self, distance):
+        consumption = (self.fuel_consumption + Truck.CONDITIONER_ON) * distance
+
+        if self.fuel_quantity >= consumption:
+            self.fuel_quantity -= consumption
+
+    def refuel(self, fuel):
+        self.fuel_quantity += fuel * 0.95
