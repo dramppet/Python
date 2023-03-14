99a72c440ed6551a06d4aa3baef19f92e74c05f7
Dragomir Petkov <dramppet@abv.bg>
2023-03-14 21:48:50 +0200

    update

--- Python_OOP/6_SOLID/lab/05_DIP/print_books.py
+++ Python_OOP/6_SOLID/lab/05_DIP/print_books.py
@@ -1,6 +1,15 @@
+from abc import ABC, abstractmethod
+
+
 class Book:
     def __init__(self, content: str):
         self.content = content
+
+
+class IFormatter(ABC):
+    @abstractmethod
+    def format(self, book: Book) -> str:
+        pass
 
 
 class Formatter:
@@ -8,7 +17,16 @@ class Formatter:
         return book.content
 
 
+class IPrinter(ABC):
+    @abstractmethod
+    def get_book(self, book: Book):
+        pass
+
+
 class Printer:
+    def __init__(self, formatter: IFormatter):
+        self.formatter = formatter
+
     def get_book(self, book: Book):
         formatter = Formatter()
         formatted_book = formatter.format(book)
