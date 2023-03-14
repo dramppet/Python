810e5cce95ea4101e5cb70c3b77c5a13b485532d
Dragomir Petkov <dramppet@abv.bg>
2023-03-14 20:37:18 +0200

    update

--- Python_OOP/6_SOLID/lab/02_OCP/animal.py
+++ Python_OOP/6_SOLID/lab/02_OCP/animal.py
@@ -2,19 +2,33 @@ class Animal:
     def __init__(self, species):
         self.species = species
 
-    def get_species(self):
-        return self.species
+    def make_sound(self):
+        pass
+
+class Cat(Animal):
+    def make_sound(self):
+        return 'meow'
+
+class Dog(Animal):
+    
+    def __init__(self, species):
+        super().__init__(species)
+    def make_sound(self):
+        return 'woof-woof'
+
+class Chicken(Animal):
+    def __init__(self, species):
+        super().__init__(species)
+    def make_sound(self):
+        return 'cluck'
 
 
 def animal_sound(animals: list):
     for animal in animals:
-        if animal.species == 'cat':
-            print('meow')
-        elif animal.species == 'dog':
-            print('woof-woof')
+        print(animal.make_sound())
 
 
-animals = [Animal('cat'), Animal('dog')]
+animals = [Cat('cat'), Dog('dog')]
 animal_sound(animals)
 
 ## добавете ново животно и рефакторирайте кода да работи без да се налага да се правят промени по него
