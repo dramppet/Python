1cb5b8184cc68f3f8eed82223f80ef73fd6c744c
Dragomir Petkov <dramppet@abv.bg>
2023-03-20 20:13:23 +0200

    update

--- Python_OOP/8_iterators_and_generators/ex/P03_countdown_iterator.py
+++ Python_OOP/8_iterators_and_generators/ex/P03_countdown_iterator.py
@@ -6,7 +6,7 @@ class countdown_iterator:
         return self
 
     def __next__(self):
-        if self.count < 0:
+        if self.count <= 0:
             raise StopIteration
         self.count -= 1
         return self.count
