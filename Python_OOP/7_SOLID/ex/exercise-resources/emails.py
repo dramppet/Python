94c9f1656bcb5ef6825330d8a65363273d53d22e
Dragomir Petkov <dramppet@abv.bg>
2023-03-24 08:21:13 +0200

    update

--- Python_OOP/7_SOLID/ex/exercise-resources/emails.py
+++ Python_OOP/7_SOLID/ex/exercise-resources/emails.py
@@ -1,7 +1,7 @@
-from abc import ABCMeta, abstractmethod
+from abc import ABC, abstractmethod
 
-class IEmail(object):
-    __metaclass__ = ABCMeta
+
+class IEmail(ABC):
 
     @abstractmethod
     def set_sender(self, sender):
@@ -15,11 +15,30 @@ class IEmail(object):
     def set_content(self, content):
         pass
 
+
+class IContent(ABC):
+
+    def __init__(self, content):
+        self.content = content
+
+    @abstractmethod
+    def content_type(self):
+        pass
+
+    def __repr__(self):
+        return '\n'.join([self.content_type(), self.content, self.content_type()])
+
+
+class MyContent(IContent):
+
+    def content_type(self):
+        return '<myML>'
+
+
 class Email(IEmail):
 
-    def __init__(self, protocol, content_type):
+    def __init__(self, protocol):
         self.protocol = protocol
-        self.content_type = content_type
         self.__sender = None
         self.__receiver = None
         self.__content = None
@@ -36,22 +55,20 @@ class Email(IEmail):
         else:
             self.__receiver = receiver
 
-    def set_content(self, content):
-        if self.content_type == 'MyML':
-            self.__content = '\n'.join(['<myML>', content, '</myML>'])
-        else:
-            self.__content = content
+    def set_content(self, content: IContent):
+
+        self.__content = content.__repr__()
 
     def __repr__(self):
 
         template = "Sender: {sender}\nReceiver: {receiver}\nContent:\n{content}"
 
-        return template.format(sender = self.__sender, receiver = self.__receiver, content = self.__content)
+        return template.format(sender=self.__sender, receiver=self.__receiver, content=self.__content)
 
 
-email = Email('IM', 'MyML')
+email = Email('IM')
 email.set_sender('qmal')
 email.set_receiver('james')
-email.set_content('Hello, there!')
+content = MyContent('Hello, there!')
+email.set_content(content)
 print(email)
-
