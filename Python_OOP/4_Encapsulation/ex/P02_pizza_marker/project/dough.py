0c1cc9560f8f73b41efc36024e6a9f1f0a24f657
Dragomir Petkov <dramppet@abv.bg>
2023-03-25 17:09:53 +0200

    update

--- Python_OOP/4_Encapsulation/ex/P02_pizza_marker/project/dough.py
+++ Python_OOP/4_Encapsulation/ex/P02_pizza_marker/project/dough.py
@@ -1,33 +1,38 @@
 class Dough:
-    def __init__(self, flour_type, baking_technique, weight):
+    def __init__(self, flour_type: str, baking_technique: str, weight: float):
         self.flour_type = flour_type
         self.baking_technique = baking_technique
-        self.weight  = weight
+        self.weight = weight
 
-        @property
-        def flour_type(self):
-            return self.__flour_type
+    @property
+    def flour_type(self):
+        return self.__flour_type
 
-        @flour_type.setter
-        def flour_type(self, value):
-            if value == '':
-                raise ValueError("The flour type cannot be an empty string")
-            self.__flour_type = value
+    @flour_type.setter
+    def flour_type(self, value):
+        if value == '':
+            raise ValueError("The flour type cannot be an empty string")
 
-        @property
-        def baking_technique(self):
-            return self.__baking_technique
+        self.__flour_type = value
 
-        @baking_technique.setter
-        def baking_technique(self, value):
-            if value == '':
-                raise ValueError("The baking technique cannot be an empty string")
+    @property
+    def baking_technique(self):
+        return self.__baking_technique
 
-        @property
-        def weight(self):
-            return self.__weight
+    @baking_technique.setter
+    def baking_technique(self, value):
+        if value == '':
+            raise ValueError("The baking technique cannot be an empty string")
 
-        @weight.setter
-        def weight(self, value):
-            if value <= 0:
-                raise ValueError("The weight cannot be less or equal to zero")
+        self.__baking_technique = value
+
+    @property
+    def weight(self):
+        return self.__weight
+
+    @weight.setter
+    def weight(self, value):
+        if value <= 0:
+            raise ValueError("The weight cannot be less or equal to zero")
+
+        self.__weight = value
